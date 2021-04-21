98364b495519b22a0cde4bf8a9ce5f5b14a584ff
Adebayo Adeyemi <yemi.adebayo.a@gmail.com>
2021-04-21 18:16:32 +0000

    Update requirements.txt to include gunicorn and Procfile.sh

--- Procfile.sh
+++ Procfile.sh
@@ -2,3 +2,4 @@
 # python3 -m venv antenv
 # source antenv/bin/activate && python -m pip install --upgrade pip && pip install -r requirements.txt
 # python manage.py makemigrations && python manage.py migrate && python manage.py runserver
+web: gunicorn azuresite.wsgi
