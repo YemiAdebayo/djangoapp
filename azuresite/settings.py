e19ff33dea25770d07c99337dd47d38d645d9427
Adebayo Adeyemi <yemi.adebayo.a@gmail.com>
2021-04-21 19:54:01 +0000

    Update requirements.txt to include gunicorn and Procfile.sh

--- azuresite/settings.py
+++ azuresite/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'azuresite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DBNAME'),
+        'USER': os.environ.get('DBUSER'),
+        'PASSWORD': os.environ.get('DBPASS'),
+        'HOST': os.environ.get('DBHOST'),
+        # 'PORT': config('DB_PORT'),
     }
 }
 
