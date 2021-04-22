62a0bedc81da52595e334fd23f40d3f36bcf196f
Adebayo Adeyemi <yemi.adebayo.a@gmail.com>
2021-04-22 19:25:12 +0000

    Update settings.py

--- azuresite/settings.py
+++ azuresite/settings.py
@@ -79,15 +79,7 @@ WSGI_APPLICATION = 'azuresite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': os.environ.get('DBNAME'),
@@ -97,6 +89,25 @@ else:
             # 'PORT': config('DB_PORT'),
         }
     }
+
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#             'NAME': os.environ.get('DBNAME'),
+#             'USER': os.environ.get('DBUSER'),
+#             'PASSWORD': os.environ.get('DBPASS'),
+#             'HOST': os.environ.get('DBHOST'),
+#             # 'PORT': config('DB_PORT'),
+#         }
+#     }
 
 
 # Password validation
