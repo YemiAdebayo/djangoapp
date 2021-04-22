1393b33515969c3fcbdacff561c59908f59f8263
Adebayo Adeyemi <yemi.adebayo.a@gmail.com>
2021-04-22 19:37:14 +0000

    Update settings.py

--- azuresite/settings.py
+++ azuresite/settings.py
@@ -134,6 +134,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
